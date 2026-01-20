102f6056a5dac275762de6c565e79123cf632023
Rojan Jafarnezhad <rojanjafarnezhad@gmail.com>
2026-01-20 17:54:09 -0500

    Add Member model and initial migration for members app

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=225)
+    lastname = models.CharField(max_length=225)
+    phone = models.IntegerField()
+    joined_date = models.DateField()
